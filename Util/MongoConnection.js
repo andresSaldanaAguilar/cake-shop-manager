98cdfd1a5317370f2685c7737dbb2ea0f85f5c68
Andres Saldana <andres.saldana.aguilar@gmail.com>
2020-11-16 10:26:35 -0600

    cleanup

--- Util/MongoConnection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports.connect = function(uri){
-    mongoose.connect(uri,{ useNewUrlParser: true,useUnifiedTopology: true})
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
-    return mongoose;
-}
